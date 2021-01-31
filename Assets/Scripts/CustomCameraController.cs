1f8419aadd35347c4f28afa3b27a52c4223a0ecd
Adam <hostinko@gmail.com>
2021-01-31 12:39:30 +0100

    zoom
    zoom pomocou klaves

--- Assets/Scripts/CustomCameraController.cs
+++ Assets/Scripts/CustomCameraController.cs
@@ -12,8 +12,12 @@ public class CustomCameraController : MonoBehaviour
     // nie som si uplne isty ci to chcem robit takto
     // mozno by bolo vhodne aby sa tieto 4 atributy ziskavali z lvl managera
     // pripadne ak by velkost levelov bola konstantna moze to zostat aj tu
+    // clamp rig position
     public Vector3 maxClampPos;
     public Vector3 minClampPos;
+    // clamp camera local position
+    public Vector3 minClampZoom;
+    public Vector3 maxClampZoom;
 
 
 
@@ -26,9 +30,9 @@ public class CustomCameraController : MonoBehaviour
     [SerializeField] Vector3 zoomAmount;
     float movementSpeed;
 
-    public Vector3 newPos;
+    Vector3 newPos;
     Quaternion newRotation;
-    public Vector3 newZoom;
+    Vector3 newZoom;
 
     Vector3 dragStartPos;
     Vector3 dragCurrentPos;
@@ -56,48 +60,39 @@ public class CustomCameraController : MonoBehaviour
 
     private void HandleKeyMovement()
     {
-        // Pomocny vector na clampovanie pozicie a zoomu
-        Vector3 tmp;
-        
+
         if (Input.GetButton("Camera Speed")) movementSpeed = fastSpeed;
         else movementSpeed = normalSpeed;
 
         // Aby system reagoval na zmenu polohy abs. hodnota musí byt vacsia ako 0 (epsilon je 0 pri porovnávaní floating point cisel)
-        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > Mathf.Epsilon || Mathf.Abs(Input.GetAxisRaw("Horizontal")) > Mathf.Epsilon)
+        if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
             //(Verical position change * speed) + (horizontal position change * speed)
             newPos += (transform.forward * Input.GetAxisRaw("Vertical") * movementSpeed) + (transform.right * Input.GetAxisRaw("Horizontal") * movementSpeed);
-            newPos = ClampVector(newPos, minClampPos, maxClampPos);
+            newPos = ClampVector3(newPos, minClampPos, maxClampPos);
             
         }
 
-        if (Mathf.Abs(Input.GetAxisRaw("Camera Rotation")) > Mathf.Epsilon )
+        if (Input.GetAxisRaw("Camera Rotation") != 0 )
         {
             newRotation *= Quaternion.Euler(Vector3.up * Input.GetAxisRaw("Camera Rotation") * rotationAmount);            
         }
 
-        if (Mathf.Abs(Input.GetAxisRaw("Camera Zoom")) > Mathf.Epsilon)
+        if (Input.GetAxisRaw("Camera Zoom") != 0)
         {
-            if (Input.GetAxisRaw("Camera Zoom") > Mathf.Epsilon) newZoom += zoomAmount;
+            if (Input.GetAxisRaw("Camera Zoom") > 0) newZoom += zoomAmount;
             else newZoom -= zoomAmount;
+            ClampVector3(newZoom, minClampZoom, maxClampZoom);
         }
-        if (Vector3.Distance(transform.position, newPos) > 1f)
-        {
-            tmp = ClampVector(Vector3.Lerp(transform.position, newPos, Time.deltaTime * movementTime), minClampPos, maxClampPos);
-            transform.position = tmp;
-            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * movementTime);
-        }
+
         
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * movementTime);        
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
-       // tmp = ClampVector(Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime), minClampPos, maxClampPos);
-        //cameraTransform.position = tmp;
-        
-
-
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
 
     }
 
-    Vector3 ClampVector(Vector3 actualVector, Vector3 min, Vector3 max)
+    Vector3 ClampVector3(Vector3 actualVector, Vector3 min, Vector3 max)
     {
         actualVector.x = Mathf.Clamp(actualVector.x, min.x, max.x);
         actualVector.y = Mathf.Clamp(actualVector.y, min.y, max.y);
