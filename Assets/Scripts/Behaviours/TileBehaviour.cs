9cbecb0dbd4c4bdfe9a8802d69d5610d174996a6
Jakub Fraňo <franojakub@gmail.com>
2021-02-11 18:45:27 +0100

    ADD: Tile prefab

---
+++ Assets/Scripts/Behaviours/TileBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehaviour : MonoBehaviour
+{
+    Tile model;
+    [SerializeField]
+    private float verticalSpawnOffset = 0.5f;
+
+    private void Awake()
+    {
+        model = new Tile(transform.position, this);
+    }
+
+    public Vector3 GetSpawnPosition()
+    {
+        //Second part will be deleted when model has pivot up in the center
+        return GetComponentInChildren<Transform>().position + new Vector3(0, verticalSpawnOffset, 0);
+    }
+}
---
+++ Assets/Scripts/Models/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    private TileBehaviour behaviour;
+    private Vector3 position;
+
+    public Tile(Vector3 position, TileBehaviour behaviour)
+    {
+        this.position = position;
+        this.behaviour = behaviour;
+    }
+}
