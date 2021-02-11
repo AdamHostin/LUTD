2d2ef3d1fab19bedc770c5bb8113b2cf8c8d975a
Jakub Fraňo <franojakub@gmail.com>
2021-02-11 18:04:01 +0100

    ADD: UnitCard prefab

---
+++ Assets/Scripts/UI/UnitCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitCard : MonoBehaviour
+{
+    [SerializeField]
+    GameObject unitPrefab;
+
+    public GameObject GetUnitPrefab()
+    {
+        return unitPrefab;
+    }
+}
