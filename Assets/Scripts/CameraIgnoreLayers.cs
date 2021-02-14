10de0ae184ae286338d16156d8aa12f46d3aa260
Jakub Fraňo <franojakub@gmail.com>
2021-02-14 20:16:30 +0100

    ADD: camera layer ignore

---
+++ Assets/Scripts/CameraIgnoreLayers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraIgnoreLayers : MonoBehaviour
+{
+    void Start()
+    {
+        Camera camera = GetComponent<Camera>();
+        camera.eventMask = LayerMask.GetMask("Default", "UI", "CInteractable");
+    }
+}
