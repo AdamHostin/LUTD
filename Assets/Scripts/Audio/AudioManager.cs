c5a06ab4129050cdb9a23ab73ce69b4a12efe887
Jakub Fraňo <franojakub@gmail.com>
2021-03-18 17:18:23 +0100

    ADD: audio management
    - sound setting in audio manager prefab

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        App.audioManager = this;
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.outputAudioMixerGroup = s.output;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Audio manager invalid sound name: " + name);
+            return;
+        }
+        s.source.Play();
+    }
+}
