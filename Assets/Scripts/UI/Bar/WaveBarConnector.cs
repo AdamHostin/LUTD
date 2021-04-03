9703a2608546cfc2d877da23d86d47659454f5ee
Adam <hostinko@gmail.com>
2021-04-03 18:25:56 +0200

    ADD: waveBar

---
+++ Assets/Scripts/UI/Bar/WaveBarConnector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveBarConnector : MonoBehaviour
+{
+    [SerializeField] private BarController waveBar;
+
+    private void OnEnable()
+    {
+        App.levelManager.waveBar = waveBar;
+    }
+}
